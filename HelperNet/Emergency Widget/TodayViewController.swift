8e3d487860f735c6b6c4e8c54ee65a610745f3b9
Willi Raschkowski <willi.raschkowski@student.hpi.de>
2015-10-03 07:01:36 +0200

    Widget

---
+++ HelperNet/Emergency Widget/TodayViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TodayViewController.swift
+//  Emergency Widget
+//
+//  Created by Willi Raschkowski on 03/10/15.
+//  Copyright © 2015 nerdishByNature. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+
+        completionHandler(NCUpdateResult.NewData)
+    }
+    
+    func widgetMarginInsetsForProposedMarginInsets(defaultMarginInsets: UIEdgeInsets) -> UIEdgeInsets {
+        return UIEdgeInsetsZero
+    }
+    
+}
